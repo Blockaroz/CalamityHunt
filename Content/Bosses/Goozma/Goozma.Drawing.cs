8c2b3b5ff9d5d3933b159f9f27e3040fe83cb523
IbanPlay <56191483+IbanPlay@users.noreply.github.com>
2023-04-17 00:21:26 +0200

    Restored the holo shader

--- Content/Bosses/Goozma/Goozma.Drawing.cs
+++ Content/Bosses/Goozma/Goozma.Drawing.cs
@@ -16,70 +16,70 @@ namespace CalamityHunt.Content.Bosses.Goozma
     //IbanPlay shader code
     public partial class Goozma : ModNPC
     {
-        //private static float maxBright;
+        private static float maxBright;
 
-        //public static float Modulo(float dividend, float divisor) => dividend - (float)Math.Floor(dividend / divisor) * divisor;
+        public static float Modulo(float dividend, float divisor) => dividend - (float)Math.Floor(dividend / divisor) * divisor;
 
-        //public static readonly Vector3[] gradientColors = new Vector3[]
-        //{
-        //    new Color(0, 0, 0).ToVector3(),
-        //    new Color(51, 46, 78).ToVector3(),
-        //    new Color(113, 53, 146).ToVector3(),
-        //    new Color(174, 23, 189).ToVector3(),
-        //    new Color(237, 128, 60).ToVector3(),
-        //    new Color(247, 255, 101).ToVector3(),
-        //    new Color(176, 234, 85).ToVector3(),
-        //    new Color(102, 219, 249).ToVector3(),
-        //    new Color(0, 0, 0).ToVector3()
-        //};
+        public static readonly Vector3[] gradientColors = new Vector3[]
+        {
+            new Color(0, 0, 0).ToVector3(),
+            new Color(51, 46, 78).ToVector3(),
+            new Color(113, 53, 146).ToVector3(),
+            new Color(174, 23, 189).ToVector3(),
+            new Color(237, 128, 60).ToVector3(),
+            new Color(247, 255, 101).ToVector3(),
+            new Color(176, 234, 85).ToVector3(),
+            new Color(102, 219, 249).ToVector3(),
+            new Color(0, 0, 0).ToVector3()
+        };
 
-        //public void GetGradientMapValues(out float[] brightnesses, out Vector3[] colors)
-        //{
-        //    maxBright = 0.66f;
-        //    brightnesses = new float[10];
-        //    colors = new Vector3[10];
+        public void GetGradientMapValues(out float[] brightnesses, out Vector3[] colors)
+        {
+            maxBright = 0.66f;
+            brightnesses = new float[10];
+            colors = new Vector3[10];
 
-        //    float rainbowStartOffset = 0.35f + NPC.localAI[0] * 0.005f;
-        //    //Calculate and store every non-modulo brightness, with the shifting offset. 
-        //    //The first brightness is ignored for the moment, it will be relevant later. Setting it to -1 temporarily
-        //    brightnesses[0] = -1;
-        //    brightnesses[1] = rainbowStartOffset + 0.35f;
-        //    brightnesses[2] = rainbowStartOffset + 0.42f;
-        //    brightnesses[3] = rainbowStartOffset + 0.47f;
-        //    brightnesses[4] = rainbowStartOffset + 0.51f;
-        //    brightnesses[5] = rainbowStartOffset + 0.56f;
-        //    brightnesses[6] = rainbowStartOffset + 0.61f;
-        //    brightnesses[7] = rainbowStartOffset + 0.64f;
-        //    brightnesses[8] = rainbowStartOffset + 0.72f;
-        //    brightnesses[9] = rainbowStartOffset + 0.75f;
+            float rainbowStartOffset = 0.35f + NPC.localAI[0] * 0.005f;
+            //Calculate and store every non-modulo brightness, with the shifting offset. 
+            //The first brightness is ignored for the moment, it will be relevant later. Setting it to -1 temporarily
+            brightnesses[0] = -1;
+            brightnesses[1] = rainbowStartOffset + 0.35f;
+            brightnesses[2] = rainbowStartOffset + 0.42f;
+            brightnesses[3] = rainbowStartOffset + 0.47f;
+            brightnesses[4] = rainbowStartOffset + 0.51f;
+            brightnesses[5] = rainbowStartOffset + 0.56f;
+            brightnesses[6] = rainbowStartOffset + 0.61f;
+            brightnesses[7] = rainbowStartOffset + 0.64f;
+            brightnesses[8] = rainbowStartOffset + 0.72f;
+            brightnesses[9] = rainbowStartOffset + 0.75f;
 
-        //    //Pass the entire rainbow through modulo 1
-        //    for (int i = 1; i < 10; i++)
-        //        brightnesses[i] = Modulo(brightnesses[i], maxBright) * maxBright;
+            //Pass the entire rainbow through modulo 1
+            for (int i = 1; i < 10; i++)
+                brightnesses[i] = Modulo(brightnesses[i], maxBright) * maxBright;
 
-        //    //Store the first element's value so we can find it again later
-        //    float firstBrightnessValue = brightnesses[1];
+            //Store the first element's value so we can find it again later
+            float firstBrightnessValue = brightnesses[1];
 
-        //    //Sort the values from lowest to highest
-        //    Array.Sort(brightnesses);
+            //Sort the values from lowest to highest
+            Array.Sort(brightnesses);
 
-        //    //Find the new index of the original first element after the list being sorted
-        //    int rainbowStartIndex = Array.IndexOf(brightnesses, firstBrightnessValue);
-        //    //Substract 1 from the index, because we are ignoring the currently negative first array slot.
-        //    rainbowStartIndex--;
+            //Find the new index of the original first element after the list being sorted
+            int rainbowStartIndex = Array.IndexOf(brightnesses, firstBrightnessValue);
+            //Substract 1 from the index, because we are ignoring the currently negative first array slot.
+            rainbowStartIndex--;
 
-        //    //9 loop, filling a list of colors in a array of 10 elements (ignoring the first one)
-        //    for (int i = 0; i < 9; i++)
-        //    {
-        //        colors[1 + (rainbowStartIndex + i) % 9] = gradientColors[i];
-        //    }
+            //9 loop, filling a list of colors in a array of 10 elements (ignoring the first one)
+            for (int i = 0; i < 9; i++)
+            {
+                colors[1 + (rainbowStartIndex + i) % 9] = gradientColors[i];
+            }
 
-        //    //We always want a brightness at index 0 to be the lower bound
-        //    brightnesses[0] = 0;
-        //    //Make the color at index 0 be a mix between the first and last colors in the list, based on the distance between the 2.
-        //    float interpolant = (1 - brightnesses[9]) / (brightnesses[1] + (1 - brightnesses[9]));
-        //    colors[0] = Vector3.Lerp(colors[9], colors[0], interpolant);
-        //}
+            //We always want a brightness at index 0 to be the lower bound
+            brightnesses[0] = 0;
+            //Make the color at index 0 be a mix between the first and last colors in the list, based on the distance between the 2.
+            float interpolant = (1 - brightnesses[9]) / (brightnesses[1] + (1 - brightnesses[9]));
+            colors[0] = Vector3.Lerp(colors[9], colors[0], interpolant);
+        }
 
         public float bend;
 
@@ -117,7 +117,9 @@ namespace CalamityHunt.Content.Bosses.Goozma
                 DrawGoozma(spriteBatch, NPC.oldPos[i] + NPC.Size * 0.5f, NPC.oldRot[i], oldVel[i], trailColor * trailStrength);
             }
 
-            //GetGradientMapValues(out float[] brightnesses, out Vector3[] colors);
+            Phase = -22;
+
+            GetGradientMapValues(out float[] brightnesses, out Vector3[] colors);
 
             //for (int i = 0; i < 8; i++)
             //{
@@ -132,9 +134,11 @@ namespace CalamityHunt.Content.Bosses.Goozma
             //    spriteBatch.Draw(flare.Value, ornamentPos - Main.screenPosition, null, new Color(ornamentColor.R, ornamentColor.G, ornamentColor.B, 0) * 0.2f, ornamentRotation + MathHelper.PiOver2, flare.Size() * 0.5f, NPC.scale * new Vector2(1f, 2f), direction, 0);
             //}
 
+
             Effect effect = ModContent.Request<Effect>($"{nameof(CalamityHunt)}/Assets/Effects/HolographEffect", AssetRequestMode.ImmediateLoad).Value;
-            effect.Parameters["uTime"].SetValue(NPC.localAI[0] * 0.01f % 1f);
-            effect.Parameters["uColorMap"].SetValue(ModContent.Request<Texture2D>($"{nameof(CalamityHunt)}/Assets/Textures/Goozma/GoozmaColorMap").Value);
+            effect.Parameters["uTime"].SetValue(NPC.localAI[0] * 0.01f % 1f); 
+            effect.Parameters["colors"].SetValue(colors);
+            effect.Parameters["brightnesses"].SetValue(brightnesses);
             effect.Parameters["baseToScreenPercent"].SetValue(1f);
             effect.Parameters["baseToMapPercent"].SetValue(0.3f - (float)Math.Sin(NPC.localAI[0] * 0.01f % MathHelper.TwoPi) * 0.3f);
             if (Phase == 3)
@@ -144,13 +148,13 @@ namespace CalamityHunt.Content.Bosses.Goozma
                 effect.Parameters["baseToScreenPercent"].SetValue(1f - fadeDownProg);
             }
 
-            //spriteBatch.End();
-            //spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, effect, Main.Transform);
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, effect, Main.Transform);
 
             DrawGoozma(spriteBatch, NPC.Center, NPC.rotation, NPC.velocity, Color.Lerp(drawColor, Color.White, 0.3f));
             
-            //spriteBatch.End();
-            //spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.Transform);
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.Transform);
             
             Vector2 crownPos = NPC.Center + drawOffset - new Vector2(6 * NPC.direction, 44).RotatedBy(extraTilt * 0.8f + NPC.rotation) * NPC.scale;
             spriteBatch.Draw(crownMask.Value, crownPos - Main.screenPosition, null, Color.White, extraTilt + NPC.rotation, crownMask.Size() * new Vector2(0.5f, 1f), 1f, direction, 0);
