f186dada6b860a301500b471f7267ef1760fa3d9
Blockaroz <lbpk.123fan@gmail.com>
2023-04-23 11:14:08 -0400

    dont tell!

--- Common/Players/ShogunArmorPlayer.cs
+++ Common/Players/ShogunArmorPlayer.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using CalamityHunt.Common.Systems.Particles;
+using CalamityHunt.Content.Particles;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,7 @@ namespace CalamityHunt.Common.Players
         public bool active;
         public float slamPower;
         private bool slamming;
+        private int bunnyHopCounter;
 
         public override void PostUpdateRunSpeeds()
         {
@@ -34,14 +37,15 @@ namespace CalamityHunt.Common.Players
                 {
                     Main.SetCameraLerp(0.2f, 20);
 
-                    Player.gravity *= 0.6f;
+                    Player.gravity *= 0.1f;
                     Player.fullRotation += Player.direction * 0.5f;
                     Player.fullRotationOrigin = Player.Size * 0.5f;
                     Player.velocity *= 1.006f;
                     for (int i = 0; i < 6; i++)
-                    {
                         Dust.NewDustPerfect(Player.Center + Main.rand.NextVector2Circular(25, 25), DustID.TintableDust, Player.velocity * -Main.rand.NextFloat(-0.5f, 1f), 100, Color.Black, 1f + Main.rand.NextFloat(1.5f)).noGravity = true;
-                    }
+
+                    Particle.NewParticle(Particle.ParticleType<HueLightDust>(), Player.Center + Main.rand.NextVector2Circular(25, 25), Player.velocity * -Main.rand.NextFloat(-0.6f, 0.6f), Player.hairColor, 0.5f + Main.rand.NextFloat());
+
                 }
                 else if (slamPower > 0)
                 {
@@ -63,11 +67,32 @@ namespace CalamityHunt.Common.Players
 
                 if (slamming)
                 {
-                    Player.velocity.X *= 0.1f;
+                    bunnyHopCounter += 20;
                     for (int i = 0; i < 40; i++)
-                        Dust.NewDustPerfect(Player.Bottom + Main.rand.NextVector2Circular(30, 5), DustID.TintableDust, -Vector2.UnitY.RotatedByRandom(1f) * Main.rand.NextFloat(7f) * (i / 40f) + new Vector2(Player.velocity.X * 2f, 0f), 100, Color.Black, 1f + Main.rand.NextFloat(1.5f)).noGravity = true;
+                        Dust.NewDustPerfect(Player.Bottom + Main.rand.NextVector2Circular(20, 5), DustID.TintableDust, Main.rand.NextVector2Circular(10, 1) - Vector2.UnitY * Main.rand.NextFloat(5f), 100, Color.Black, 1f + Main.rand.NextFloat(1.5f)).noGravity = true;
+
+                    for (int i = 0; i < 5; i++)
+                        Particle.NewParticle(Particle.ParticleType<HueLightDust>(), Player.Bottom + Main.rand.NextVector2Circular(30, 5), Main.rand.NextVector2Circular(6, 1) - Vector2.UnitY * Main.rand.NextFloat(2f), Player.hairColor, 1f);
 
                     slamPower = 0;
+                }
+
+                if (bunnyHopCounter > 0)
+                {
+                    bunnyHopCounter--;
+
+                    if (Player.controlJump)
+                    {
+                        bunnyHopCounter = 0;
+                        Player.velocity.X *= 2f;
+
+                        for (int i = 0; i < 40; i++)
+                            Dust.NewDustPerfect(Player.Bottom + Main.rand.NextVector2Circular(20, 5), DustID.TintableDust, -Vector2.UnitY.RotatedByRandom(1f) * Main.rand.NextFloat(7f) * (i / 40f) - new Vector2(Player.direction * 10f, 0f), 100, Color.Black, 1f + Main.rand.NextFloat(1.5f)).noGravity = true;
+
+                        for (int i = 0; i < 5; i++)
+                            Particle.NewParticle(Particle.ParticleType<HueLightDust>(), Player.Bottom + Main.rand.NextVector2Circular(20, 5), -Vector2.UnitY.RotatedByRandom(1f) * Main.rand.NextFloat(2f) * (i / 40f) - new Vector2(Player.direction * 10f, 0f), Player.hairColor, 0.5f + Main.rand.NextFloat());
+
+                    }
                 }
 
                 if (slamPower > 0f)
@@ -76,6 +101,7 @@ namespace CalamityHunt.Common.Players
             else
             {
                 Player.fullRotation = 0;
+                bunnyHopCounter = 0;
                 slamPower = 0;
             }
         }
