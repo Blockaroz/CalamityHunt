5c8c0bfcb1de5edde072d95b2cacce4507ade6ce
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2023-05-21 12:33:39 -0500

    restoration potion and new buff sprites

---
+++ Content/Items/Consumable/SupremeRestorationPotion.cs
@@ -0,0 +1,41 @@
+﻿using CalamityHunt.Content.Items.Materials;
+using CalamityHunt.Content.Particles;
+using Microsoft.Xna.Framework;
+using CalamityHunt.Common.Systems.Particles;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityHunt.Content.Items.Consumable
+{
+    public class SupremeRestorationPotion : ModItem
+    {
+        public static LocalizedText RestoreLifeText { get; private set; }
+
+        public override void SetStaticDefaults()
+        {
+            RestoreLifeText = this.GetLocalization(nameof(RestoreLifeText));
+
+            Item.ResearchUnlockCount = 30;
+        }
+
+        public override void SetDefaults()
+        {
+			Item.width = 20;
+			Item.height = 26;
+			Item.useStyle = ItemUseStyleID.DrinkLiquid;
+			Item.useAnimation = 17;
+			Item.useTime = 17;
+			Item.useTurn = true;
+			Item.UseSound = SoundID.Item3;
+			Item.maxStack = 30;
+			Item.consumable = true;
+			Item.rare = ItemRarityID.Orange;
+			Item.value = Item.buyPrice(gold: 1);
+
+			Item.healLife = 225; // While we change the actual healing value in GetHealLife, Item.healLife still needs to be higher than 0 for the item to be considered a healing item
+			Item.potion = true; // Makes it so this item applies potion sickness on use and allows it to be used with quick heal
+		}
+    }
+}
