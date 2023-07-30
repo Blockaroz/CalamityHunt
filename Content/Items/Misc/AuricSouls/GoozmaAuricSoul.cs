80cc20902940b9062edce15c6f8e1bc59ebd1b61
Blockaroz <lbpk.123fan@gmail.com>
2023-07-30 12:33:08 -0400

    soul

--- Content/Items/Misc/AuricSouls/GoozmaAuricSoul.cs
+++ Content/Items/Misc/AuricSouls/GoozmaAuricSoul.cs
@@ -18,6 +18,7 @@ using Terraria.GameContent;
 using Terraria.Graphics;
 using Terraria.ID;
 using Terraria.ModLoader;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CalamityHunt.Content.Items.Misc.AuricSouls
 {
@@ -117,11 +118,6 @@ namespace CalamityHunt.Content.Items.Misc.AuricSouls
 
         public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
         {
-            bool includeEye = true;
-            bool includeTrails = true;
-
-            Texture2D texture = TextureAssets.Item[Type].Value;
-            Texture2D sparkTexture = AssetDirectory.Textures.Sparkle;
             Texture2D glowTexture = AssetDirectory.Textures.GlowBig;
             Texture2D eyeTexture = AssetDirectory.Textures.Extras.GoozmaGodEye;
 
@@ -131,95 +127,76 @@ namespace CalamityHunt.Content.Items.Misc.AuricSouls
             float soulScale = scale;
             float fastScale = 1f + MathF.Sin(Main.GlobalTimeWrappedHourly * 25f % MathHelper.TwoPi) * 0.1f;
             scale = 1f + MathF.Sin(Main.GlobalTimeWrappedHourly * 3f % MathHelper.TwoPi) * 0.1f;
+           
+            spriteBatch.Draw(glowTexture, Item.Center - Main.screenPosition, glowTexture.Frame(), Color.Black * 0.2f, 0, glowTexture.Size() * 0.5f, soulScale * 3f, 0, 0);
 
-            //spriteBatch.Draw(sparkTexture, Item.Center - Main.screenPosition, sparkTexture.Frame(), Color.Black * 0.1f, -MathHelper.PiOver4, sparkTexture.Size() * 0.5f, scale * new Vector2(3f, 0.8f), 0, 0);
-            //spriteBatch.Draw(sparkTexture, Item.Center - Main.screenPosition, sparkTexture.Frame(), Color.Black * 0.1f, MathHelper.PiOver4, sparkTexture.Size() * 0.5f, scale * new Vector2(3f, 0.8f), 0, 0);
-            //spriteBatch.Draw(sparkTexture, Item.Center - Main.screenPosition, sparkTexture.Frame(), Color.Black * 0.1f, 0, sparkTexture.Size() * 0.5f, scale * new Vector2(3f, 0.8f), 0, 0);
-            //spriteBatch.Draw(sparkTexture, Item.Center - Main.screenPosition, sparkTexture.Frame(), Color.Black * 0.1f, MathHelper.PiOver2, sparkTexture.Size() * 0.5f, scale * new Vector2(3f, 0.8f), 0, 0);
+            VertexStrip strip = new VertexStrip();
 
-            if (includeTrails)
+            int count = 500;
+            Vector2[] offs = new Vector2[count];
+            float[] offRots = new float[count];
+
+            float time = Main.GlobalTimeWrappedHourly * 0.5f;
+            for (int i = 0; i < count; i++)
             {
-                VertexStrip strip1 = new VertexStrip();
-                VertexStrip strip2 = new VertexStrip();
-                VertexStrip strip3 = new VertexStrip();
-
-                int count = 120;
-                Vector2[] offs1 = new Vector2[count];
-                Vector2[] offs2 = new Vector2[count];
-                Vector2[] offs3 = new Vector2[count];
-                float[] offRots1 = new float[count];
-                float[] offRots2 = new float[count];
-                float[] offRots3 = new float[count];
-
-                float time = Main.GlobalTimeWrappedHourly * 2f;
-                for (int i = 0; i < count; i++)
-                {
-                    Vector2 x = new Vector2(35 + MathF.Sin(time + i * 0.02f) * 10f, 0).RotatedBy(MathHelper.TwoPi / 120f * i - time);
-                    x.X *= 1f + MathF.Cos(time * 1.5f) * 0.2f;
-                    Vector2 y = new Vector2(100 + MathF.Cos(time + i * 0.07f) * 25f, 0).RotatedBy(MathHelper.TwoPi / 130f * i + time * 0.5f + 1f);
-                    y.Y *= 0.4f + MathF.Sin(time * 0.1f) * 0.1f;
-                    Vector2 z = new Vector2(80 + MathF.Cos(time - i * 0.01f) * 10f, 0).RotatedBy(MathHelper.TwoPi / 150f * i + time * 2f + 2f);
-                    z.Y *= 0.6f + MathF.Sin(time) * 0.2f;
-                    offs1[i] = x.RotatedBy(time * 2f) * 0.7f;
-                    offs2[i] = y.RotatedBy(time * 0.3f) * 0.5f;
-                    offs3[i] = z.RotatedBy(-time * 0.5f) * new Vector2(1f, 0.8f) * 0.4f;
-
-                    Vector2 f = new Vector2(20 + time * 0.4f % 1f * 150f, 0).RotatedBy(MathHelper.TwoPi / 80f * i - time + 2f);
-                    f.Y *= 1f + MathF.Sin(time * 2f) * 0.1f;
-                }
-
-                offRots1[0] = offs1[0].AngleTo(offs1[1]);
-                offRots2[0] = offs2[0].AngleTo(offs2[1]);
-                offRots3[0] = offs2[0].AngleTo(offs2[1]);
-                for (int i = 1; i < count; i++)
-                {
-                    offRots1[i] = offs1[i - 1].AngleTo(offs1[i]);
-                    offRots2[i] = offs2[i - 1].AngleTo(offs2[i]);
-                    offRots3[i] = offs3[i - 1].AngleTo(offs3[i]);
-                }
-
-                Color StripColor(float p)
-                {
-                    Color final = new GradientColor(SlimeUtils.GoozColors, 0.5f, 0.5f).ValueAt(p * 20 - 10 + Main.GlobalTimeWrappedHourly * 30);
-                    return final * 0.9f;
-                }
-                float StripWidth(float p) => Utils.GetLerpValue(0f, 0.5f, p, true) * Utils.GetLerpValue(1f, 0.5f, p, true) * 20f;
-
-                strip1.PrepareStrip(offs1, offRots1, StripColor, StripWidth, Item.Center - Main.screenPosition, offs1.Length, true);
-                strip2.PrepareStrip(offs2, offRots2, StripColor, StripWidth, Item.Center - Main.screenPosition, offs2.Length, true);
-                strip3.PrepareStrip(offs3, offRots3, StripColor, StripWidth, Item.Center - Main.screenPosition, offs3.Length, true);
-
-                Effect effect = ModContent.Request<Effect>($"{nameof(CalamityHunt)}/Assets/Effects/GooLightningEffect", AssetRequestMode.ImmediateLoad).Value;
-                effect.Parameters["uTransformMatrix"].SetValue(Main.GameViewMatrix.NormalizedTransformationmatrix);
-                effect.Parameters["uTexture"].SetValue(trailTexture);
-                effect.Parameters["uGlow"].SetValue(TextureAssets.Extra[197].Value);
-                effect.Parameters["uColor"].SetValue(Vector3.One);
-                effect.Parameters["uTime"].SetValue(Main.GlobalTimeWrappedHourly * 0.66f % 1f);
-                effect.Parameters["uBackPower"].SetValue(0.6f);
-                effect.CurrentTechnique.Passes[0].Apply();
-
-                strip1.DrawTrail();
-                strip2.DrawTrail();
-                strip3.DrawTrail();
+                Vector2 x = new Vector2(50 + MathF.Sin(time * 2 + i / (float)count * 76f) * 7f, 0).RotatedBy(MathHelper.TwoPi / (float)count * i - time);
+                x.X *= 1f + MathF.Cos(time * 1.5f) * 0.1f;
+                offs[i] = x.RotatedBy(time);
             }
 
-            if (includeEye)
-            {
-                spriteBatch.Draw(eyeTexture, Item.Center - Main.screenPosition, eyeTexture.Frame(), GetAlpha(Color.White).Value * 0.3f, -MathHelper.PiOver4, eyeTexture.Size() * 0.5f, scale * 0.8f, 0, 0);
-                spriteBatch.Draw(eyeTexture, Item.Center - Main.screenPosition, eyeTexture.Frame(), glowColor, -MathHelper.PiOver4, eyeTexture.Size() * 0.5f, scale * 0.8f, 0, 0);
-                spriteBatch.Draw(eyeTexture, Item.Center - Main.screenPosition, eyeTexture.Frame(), new Color(100, 100, 100, 0), -MathHelper.PiOver4, eyeTexture.Size() * 0.5f, scale * 0.8f, 0, 0);
-            }
-            Rectangle frame = Main.itemAnimations[Type].GetFrame(texture, Main.itemFrameCounter[whoAmI]);
-            spriteBatch.Draw(texture, Item.Center - Main.screenPosition, frame, GetAlpha(Color.White).Value, 0, frame.Size() * 0.5f, soulScale * 0.6f, 0, 0);
-            spriteBatch.Draw(texture, Item.Center - Main.screenPosition, frame, new Color(200, 200, 200, 0), 0, frame.Size() * 0.5f, soulScale * 0.6f, 0, 0);
+            offRots[0] = offs[0].AngleTo(offs[1]);
 
-            spriteBatch.Draw(glowTexture, Item.Center - Main.screenPosition, glowTexture.Frame(), glowColor * 0.2f, 0, glowTexture.Size() * 0.5f, scale * 0.4f, 0, 0);
+            for (int i = 1; i < count; i++)
+                offRots[i] = offs[i - 1].AngleTo(offs[i]);
+
+            Color StripColor(float p)
+            {
+                Color final = new GradientColor(SlimeUtils.GoozColors, 0.5f, 0.5f).ValueAt(p * 20 - 10 + Main.GlobalTimeWrappedHourly * 30);
+                return final * 0.9f;
+            }
+            float StripWidth(float p) => 16f * MathHelper.Clamp(MathF.Sin(p * 20) + 0.4f, 0f, 1f);
+
+            strip.PrepareStrip(offs, offRots, StripColor, StripWidth, Item.Center - Main.screenPosition, offs.Length, true);
+
+            Effect effect = ModContent.Request<Effect>($"{nameof(CalamityHunt)}/Assets/Effects/GooLightningEffect", AssetRequestMode.ImmediateLoad).Value;
+            effect.Parameters["uTransformMatrix"].SetValue(Main.GameViewMatrix.NormalizedTransformationmatrix);
+            effect.Parameters["uTexture"].SetValue(TextureAssets.Extra[197].Value);
+            effect.Parameters["uGlow"].SetValue(TextureAssets.Extra[197].Value);
+            effect.Parameters["uColor"].SetValue(Vector3.One);
+            effect.Parameters["uTime"].SetValue(Main.GlobalTimeWrappedHourly * 0.66f % 1f);
+            effect.Parameters["uBackPower"].SetValue(1f);
+            effect.CurrentTechnique.Passes[0].Apply();
+
+            strip.DrawTrail();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Color sparkleColor = new GradientColor(SlimeUtils.GoozColors, 0.5f, 0.5f).ValueAt(Main.GlobalTimeWrappedHourly * 30 + i);
+                sparkleColor.A = 0;
+                float length = 4f;
+                if (i % 2 == 1)
+                    length *= 0.5f;
+                DrawSideSparkle(spriteBatch, Item.Center + new Vector2(10 + length * 15, 0).RotatedBy(MathHelper.PiOver2 * i) - Main.screenPosition, MathHelper.PiOver2 * i, sparkleColor, length);
+            }
+
+            spriteBatch.Draw(eyeTexture, Item.Center - Main.screenPosition, eyeTexture.Frame(), GetAlpha(Color.White).Value * 0.3f, -MathHelper.PiOver4, eyeTexture.Size() * 0.5f, scale * 0.8f, 0, 0);
+            spriteBatch.Draw(eyeTexture, Item.Center - Main.screenPosition, eyeTexture.Frame(), new Color(255, 255, 255, 0), -MathHelper.PiOver4, eyeTexture.Size() * 0.5f, scale * 0.8f, 0, 0);
+            spriteBatch.Draw(eyeTexture, Item.Center - Main.screenPosition, eyeTexture.Frame(), glowColor, -MathHelper.PiOver4, eyeTexture.Size() * 0.5f, scale * 0.9f, 0, 0);
+
             spriteBatch.Draw(glowTexture, Item.Center - Main.screenPosition, glowTexture.Frame(), glowColor * 0.4f, 0, glowTexture.Size() * 0.5f, 0.2f + fastScale * 0.4f, 0, 0);
             spriteBatch.Draw(glowTexture, Item.Center - Main.screenPosition, glowTexture.Frame(), glowColor * 0.2f, 0, glowTexture.Size() * 0.5f, fastScale, 0, 0);
-
-            spriteBatch.Draw(glowTexture, Item.Center - Main.screenPosition, glowTexture.Frame(), glowColor * 0.7f, 0, glowTexture.Size() * 0.5f, scale * 0.2f, 0, 0);
+            spriteBatch.Draw(glowTexture, Item.Center - Main.screenPosition, glowTexture.Frame(), glowColor * 0.6f, 0, glowTexture.Size() * 0.5f, 0.1f + fastScale * 0.1f, 0, 0);
 
             return false;
+        }
+
+        private void DrawSideSparkle(SpriteBatch spriteBatch, Vector2 position, float rotation, Color color, float length)
+        {
+            Texture2D sparkTexture = AssetDirectory.Textures.Sparkle;
+            Vector2 t = new Vector2(0.17f, length);
+            Vector2 l = new Vector2(0.6f, length + 0.1f);
+            spriteBatch.Draw(sparkTexture, position, sparkTexture.Frame(), color * 0.1f, rotation + MathHelper.PiOver2, sparkTexture.Size() * 0.5f, l, 0, 0);
+            spriteBatch.Draw(sparkTexture, position, sparkTexture.Frame(), color * 1.5f, rotation + MathHelper.PiOver2, sparkTexture.Size() * 0.5f, t, 0, 0);
         }
 
         public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
