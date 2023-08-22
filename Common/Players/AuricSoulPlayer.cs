e1da102a2dc1d4ecfec75c85a054ec1e8b73f532
GinYuH <>
2023-08-22 15:52:26 -0700

    fixed auric soul health + yharon soul stats

--- Common/Players/AuricSoulPlayer.cs
+++ Common/Players/AuricSoulPlayer.cs
@@ -39,9 +39,31 @@ namespace CalamityHunt.Common.Players
                 Player.moveSpeed += 0.03f;
                 Player.GetAttackSpeed(DamageClass.Melee) += 0.03f;
                 Player.endurance += 0.01f;
+                Player.statDefense += 3;
+            }
+            if (yharonSoul)
+            {
+                Player.GetDamage(DamageClass.Generic) += 0.03f;
+                Player.GetCritChance(DamageClass.Generic) += 0.03f;
+                Player.GetKnockback(DamageClass.Generic) += 0.03f;
+                Player.moveSpeed += 0.03f;
+                Player.GetAttackSpeed(DamageClass.Melee) += 0.03f;
+                Player.endurance += 0.01f;
+                Player.statDefense += 3;
+            }
+        }
+
+        public override void ResetEffects()
+        {
+            if (goozmaSoul)
+            {
                 Player.statLifeMax2 += 10;
                 Player.statManaMax2 += 20;
-                Player.statDefense += 3;
+            }
+            if (yharonSoul)
+            {
+                Player.statLifeMax2 += 10;
+                Player.statManaMax2 += 20;
             }
         }
     }
