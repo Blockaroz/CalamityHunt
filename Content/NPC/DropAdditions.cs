b6305ecb9aecb06a1a607762976a1ea1743e1e73
GinYuH <chubbeanstolfo@yahoo.com>
2023-09-13 22:37:56 -0700

    there's goo in your hair

--- Content/NPC/DropAdditions.cs
+++ Content/NPC/DropAdditions.cs
@@ -1,4 +1,6 @@
 ﻿using CalamityHunt.Common.DropRules;
+using CalamityHunt.Common.Systems;
+using CalamityHunt.Content.Items.Dyes;
 using CalamityHunt.Content.Items.Misc;
 using CalamityHunt.Content.Items.Misc.AuricSouls;
 using Terraria;
@@ -11,6 +13,17 @@ namespace CalamityHunt.Content.NPCs
     public class DropAdditions : GlobalNPC
     {
         public override bool InstancePerEntity => true;
+
+        public static Condition goozDown = new("After Goozma has been defeated", () => ModContent.GetInstance<BossDownedSystem>().GoozmaDowned);
+
+        public override void ModifyShop(NPCShop shop)
+        {
+            int type = shop.NpcType;
+            if (type == NPCID.Stylist)
+            {
+                shop.Add(ModContent.ItemType<GoopHairDye>(), goozDown);
+            }
+        }
 
         public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
         {
