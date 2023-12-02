813364a365ff210c0513b49642dbdc3f84f5d53a
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2023-12-02 21:46:43 +0200

    goozboulder

---
+++ Content/Items/Placeable/GoozmaBoulderItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalamityHunt.Content.Items.Materials;
+using CalamityHunt.Content.Items.Rarities;
+using CalamityHunt.Content.Tiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityHunt.Content.Items.Placeable
+{
+    public class GoozmaBoulderItem : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<GoozmaBoulderTile>());
+            Item.width = 46;
+            Item.height = 46;
+            Item.rare = ModContent.RarityType<VioletRarity>();
+        }
+
+        public override void AddRecipes()
+        {
+            if (ModLoader.HasMod("CalamityMod")) {
+                Mod calamity = ModLoader.GetMod("CalamityMod");
+                CreateRecipe()
+                    .AddIngredient(ItemID.Boulder)
+                    .AddIngredient<ChromaticMass>(5)
+                    .AddTile(calamity.Find<ModTile>("DraedonsForge").Type)
+                    .AddCondition(Condition.InGraveyard)
+                    .Register();
+            }
+            else {
+                CreateRecipe()
+                    .AddIngredient(ItemID.Boulder)
+                    .AddIngredient<ChromaticMass>(5)
+                    .AddTile(TileID.HeavyWorkBench)
+                    .AddCondition(Condition.InGraveyard)
+                    .Register();
+            }
+        }
+    }
+}
